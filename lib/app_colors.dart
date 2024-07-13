9a59f6eaf0ff76a9fcc46126eab3b93487de3eb4
ahmed abuzaid <ahmedabuzaid130@gmail.com>
2024-07-13 06:40:21 +0300

    Finished design radio tab and sebha tab.

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const blackColor = Color(0xff242424);
   static const whiteColor = Color(0xffffffff);
   static const primaryLightColor = Color(0xffB7935F);
+  static const secondLightColor = Color(0xffbda286);
 }
