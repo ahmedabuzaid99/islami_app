d0cebc2b8334be02c27c6ee33f1626473f96c542
ahmed abuzaid <ahmedabuzaid130@gmail.com>
2024-07-13 04:57:37 +0300

    Finished hadeth tab. Added hadeth details screen , item hadeth details.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/hadeth/hadeth_details_screen.dart';
 import 'package:islami_app/home/home_screen.dart';
 import 'package:islami_app/my_theme_data.dart';
 import 'package:islami_app/quran/sura_details_screen.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
         SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
+        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen(),
       },
       theme: MyThemeData.LightMode,
     );
