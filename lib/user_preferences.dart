adcd57897885ac2866f939ed7d8a7e786f3567ed
ahmed abuzaid <ahmedabuzaid130@gmail.com>
2024-07-23 23:36:41 +0300

    Finished shared preferences the app .

---
+++ lib/user_preferences.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreferences {
+  static late SharedPreferences _preferences;
+  static const _language = "language";
+  static const _theme = 'theme';
+
+  static Future init() async =>
+      _preferences = await SharedPreferences.getInstance();
+
+  static Future setLanguage(String language) async =>
+      await _preferences.setString(_language, language);
+
+  static String getLanguage() => _preferences.getString(_language).toString();
+
+  static Future saveThemePreference(ThemeMode themeMode) async {
+    String name;
+    if (themeMode == ThemeMode.light) {
+      name = 'light';
+    } else {
+      name = 'dark';
+    }
+    _preferences.setString(_theme, name);
+  }
+
+  static ThemeMode getThemePreference() {
+    String theme = _preferences.getString(_theme).toString();
+
+    if (theme == 'light') {
+      return ThemeMode.light;
+    } else {
+      return ThemeMode.dark;
+    }
+  }
+}
